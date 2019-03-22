c87b790f684c4825d48c959e9fe3e413c17d4164
Ali Mehdi <salimehdi144@gmail.com>
2019-03-22 17:27:52 +0500

    Update multilinear.py

--- multilinear.py
+++ multilinear.py
@@ -53,3 +53,10 @@ X = np.append(arr = np.ones((50,1)).astype(int), values = X , axis = 1)
 X_opt = X[: , [0,1,2,3,4,5]]
 regressor_OLS = sm.OLS(endog = y , exog = X_opt).fit()
 regressor_OLS.summary()
+
+
+# Now , By eliminiating some parameters
+
+X_opt = X[: , [0,3]]
+regressor_OLS = sm.OLS(endog = y , exog = X_opt).fit()
+regressor_OLS.summary()
